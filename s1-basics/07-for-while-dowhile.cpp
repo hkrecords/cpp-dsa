e117ecbb161c4e3a52dcbae0bc26ff1e4ed870ec
himanshu <hkholiya99@gmail.com>
2024-10-27 13:35:44 +0530

    syntax - for, while, dp-while

---
+++ s1-basics/07-for-while-dowhile.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    // for
+    for (int i = 1; i <= 10; i++) {
+        cout << "Hemaan" << endl;
+    }
+
+    // while
+    int i = 1;
+    while (i <= 10) {
+        cout << "hk" << endl;
+        i++;
+    }
+
+    // do while- at least runs 1 time
+    do {
+        cout << "himanshu" << endl;
+        i++;
+    } while (i < 1);
+
+    return 0;
+}
