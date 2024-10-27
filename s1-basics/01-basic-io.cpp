7d8177cbb25afec2a71aa9db852a278b79cc2347
himanshu <hkholiya99@gmail.com>
2024-10-27 12:09:25 +0530

    basic i/o, libraries

---
+++ s1-basics/01-basic-io.cpp
@@ -0,0 +1,48 @@
+// We have to use library to use different operations like input, output, maths, etc.
+
+// #include<math.h> // math function library
+
+/* 
+#include<iostream> // input output library
+
+int main () {
+    std::cout << "Hey Hemaan" << "\n";
+    std::cout << "Hey Hemaan" << std::endl << "Hey hk" << std::endl;
+    return 0;
+}
+ */
+
+/* 
+// instead of writing std:: everywhere we can use 'using namespace std' -> then program using std for all the funtions automatically
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    cout << "Hey HK" << endl;
+    return 0;
+}
+ */
+
+/* 
+// input -> cin >> & output -> cout <<
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y << endl; 
+    return 0;
+}
+ */
+
+
+// if you want to include all the libraries in one -> <bits/stdc++.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    cout << "Hey HK" << endl;
+    return 0;
+}
