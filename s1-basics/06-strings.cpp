975f3ccfd789148331963b4d230dc87118c98b45
himanshu <hkholiya99@gmail.com>
2024-10-27 13:28:35 +0530

    strings basics

---
+++ s1-basics/06-strings.cpp
@@ -0,0 +1,16 @@
+// string also stores every character in terms of indices.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string s = "himanshu";
+
+    int len = s.size();
+
+    cout << s[3];
+
+    cout << s[len - 1]; // print last character of string
+
+    return 0;
+}
